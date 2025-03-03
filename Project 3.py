338abc7c648434dffd53f5f5bda2c17163ddc402
NIKHIL SINGH <nikhil1108singh@gmail.com>
2025-03-04 00:09:15 +0530

    A simple Age Finder

---
+++ Project 3.py
@@ -0,0 +1,6 @@
+age = int(input("whats your age = "))
+left_years = 70 - age
+left_m = left_years*12
+left_w = left_years*52
+left_d = left_years*365
+print(f"you have {left_d} days or {left_w} weeks or {left_m} months to live")
