389ab73c2db555bb30c3924da6470327b507804d
NIKHIL SINGH <nikhil1108singh@gmail.com>
2025-03-16 23:12:29 +0530

    Fated one

---
+++ Code_3.py
@@ -0,0 +1,11 @@
+def greet(name,location):
+    print(f"Tapstry of fate is for the {name} in {location}")
+    print("woven by us")
+    print(f"and its a unending cycle of {location} which no one can escape")
+
+a=input("Do you belive in fate = \b")
+
+if a=='y' :
+    greet("Tarnished_one","abbys")
+else:
+    print("we are not fated")
